b768df614f1695ed386c24b73e752f736453d559
Corentin VÉROT <verot.corentin@protonmail.com>
2019-10-14 15:57:15 +0200

    Ajout des traduction et variables d'env

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements',
+    'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
+
+    'home'   => 'Accueil',
+    'login' => 'Connexion',
+    'logout' => 'Se déconnecter',
+    'register' => 'Inscription',
+
+    'rememberMe' => 'Se souvenir de moi',
+    'forgottenPassword' => 'Mot de passe oublié ?',
+    'resetPassword' => 'Réinitialiser mot de passe',
+    'sentPasswordResetLink' => "Envoyer le lien à cette adresse",
+
+    'name' => 'Nom d\'utilisateur',
+    'email' => 'Adresse mail',
+    'password' => 'Mot de passe',
+    'confirmPassword' => 'Confirmation du mot de passe',
+    'phone' => 'Téléphone',
+
+    'temporary_password' => "Le mot de passe devra être redéfini par l'utilisateur",
+
+
+    // Errors
+    'username.unique' => 'Une personne avec le même nom et prénom est déjà enregistrée',
+
+    'wrongCurrentPassword' => 'Le mot de passe actuel n\'est pas bon',
+
+
+];
